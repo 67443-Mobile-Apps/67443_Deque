c09fa5c8cba3c4eb5ff5ad0769b33fcd8883f792
profh <profh@cmu.edu>
2020-09-02 12:53:32 -0400

    Playground and testing suite for deque data structure

---
+++ Deque.swift
@@ -0,0 +1,50 @@
+/*
+  Deque (pronounced 'deck') is a generalized queue 
+  where it is possible to push or pop from either 
+  the front or the back. (Just like drawing cards 
+  from the top or bottom of a deck.)  
+ 
+  Unlike a queue (which is always FIFO), we could 
+  place things in any order we desire. 
+*/
+
+public struct Deque<T> {
+  private var array = [T]()
+  
+  public var isEmpty: Bool {
+    return array.isEmpty
+  }
+  
+  public var count: Int {
+    return array.count
+  }
+  
+  public func peekFirst() -> T? {
+    return array.first
+  }
+  
+  public func peekLast() -> T? {
+    return array.last
+  }
+  
+  mutating func pushBack(_ obj: T) {
+    array.append(obj)
+  }
+  
+  mutating func pushFront(_ obj: T) {
+    array.insert(obj, at: 0)
+  }
+  
+  mutating func popBack() -> T? {
+    return array.popLast()
+  }
+  
+  mutating func popFront() -> T? {
+    if array.isEmpty {
+      return nil
+    } else {
+      return array.removeFirst()
+    }
+  }
+  
+}
